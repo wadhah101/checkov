2cc22a02b29e2a7e9d7267acffb08119f66882bf
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2023-05-09 13:43:18 +0200

    add graph check  to kustomize

---
+++ tests/kustomize/graph/test_running_graph_checks.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import pytest
+from pytest_mock import MockerFixture
+
+from checkov.kustomize.runner import Runner
+from checkov.runner_filter import RunnerFilter
+
+RESOURCES_DIR = Path(__file__).parent / "resources"
+
+
+@pytest.mark.parametrize("graph_framework", ["NETWORKX", "IGRAPH"])
+def test_runner(mocker: MockerFixture, graph_framework):
+    # given
+    test_file_path = RESOURCES_DIR
+    runner_filter = RunnerFilter( framework=["kustomize"] , checks=["CKV2_K8S_6"])
+
+    mocker.patch.dict("os.environ", {"CHECKOV_GRAPH_FRAMEWORK": graph_framework})
+
+    # when
+    report = Runner().run(root_folder=None, files=[str(test_file_path)], runner_filter=runner_filter)
+
+    #  when
+    summary = report.get_summary()
+
+    assert summary["passed"] == 1
+    assert summary["failed"] == 1
+    assert summary["skipped"] == 1
+    assert summary["parsing_errors"] == 0
